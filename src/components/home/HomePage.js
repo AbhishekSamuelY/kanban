4a39413a3f7fa711cbbdf5ab70888114c82a82b3
AbhishekSamuelY <abhishek18samuel@gmail.com>
2020-04-18 20:29:24 +0530

    Initial Commit

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {connect} from "react-redux";
+import * as cardActions from "../../redux/actions/cardActions";
+import PropTypes from "prop-types";
+import {bindActionCreators} from "redux";
+
+class HomePage extends React.Component {
+
+    state = {
+        card: {
+            title: ""
+        }
+    };
+
+    handleChange = event => {
+        const card = {
+            ...this.state.card,
+            title: event.target.value
+        };
+        this.setState({card});
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.props.actions.createCard(this.state.card);
+    }
+
+    render() {
+        return (<form onSubmit={
+            this.handleSubmit
+        }>
+            <h2>Cards</h2>
+            <h3>Add Card</h3>
+            <input type="text"
+                onChange={
+                    this.handleChange
+                }
+                value={
+                    this.state.card.title
+                }/>
+            <input type="submit" value="save"/> {
+            this.props.cards.map(card => (<div key={
+                card.title
+            }> {
+                card.title
+            }</div>))
+        } </form>);
+    }
+} HomePage.propTypes = {
+    cards: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+}
+
+function mapStateToProps(state) {
+    return {cards: state.cards};
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(cardActions, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
